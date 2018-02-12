9061f3b4c6389888f0519a4755cb48f48c02d0e6
Anne Whitman <awhitman@docs.rutgers.edu>
2018-02-11 23:36:25 -0500

    Finished song library

--- ListView/src/view/ListController.java
+++ ListView/src/view/ListController.java
@@ -1,9 +1,14 @@
 package view;
 
+import java.util.Optional;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
 
 public class ListController {
@@ -30,6 +35,52 @@ public class ListController {
 				"Seahawks",
 				"Steelers",
 				"Jaguars");               
-		listView.setItems(obsList);  
+		listView.setItems(obsList); 
+		
+		// select the first item
+		listView.getSelectionModel().select(0);
+	
+		// set listener for the items
+		listView
+		.getSelectionModel()
+		.selectedIndexProperty()
+		.addListener(
+				(obs, oldVal, newVal) -> 
+				//showItem(mainStage)
+				showItemInputDialog(mainStage)
+				);     
 	}
+	
+	public void showItem(Stage mainStage) {                
+		Alert alert = 
+				new Alert(AlertType.INFORMATION);
+		alert.initOwner(mainStage);
+		alert.setTitle("List Item");
+		alert.setHeaderText(
+				"Selected list item properties");
+
+		String content = "Index: " + 
+				listView.getSelectionModel()
+		.getSelectedIndex() + 
+		"\nValue: " + 
+		listView.getSelectionModel()
+		.getSelectedItem();
+
+		alert.setContentText(content);
+		alert.show();
+
+	}
+
+	public void showItemInputDialog(Stage mainStage) {                
+		String item = listView.getSelectionModel().getSelectedItem();
+		int index = listView.getSelectionModel().getSelectedIndex();
+
+		TextInputDialog dialog = new TextInputDialog(item);
+		dialog.initOwner(mainStage); dialog.setTitle("List Item");
+		dialog.setHeaderText("Selected Item (Index: " + index + ")");
+		dialog.setContentText("Enter name: ");
+
+		Optional<String> result = dialog.showAndWait();
+		if (result.isPresent()) { obsList.set(index, result.get()); }
+	}	
 }
--- SongLibrary/src/view/SongController.java
+++ SongLibrary/src/view/SongController.java
@@ -1,47 +1,342 @@
 package view;
 
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class SongController {
-	@FXML         
-	ListView<String> listView;                
-
-	private ObservableList<String> obsList;              
-
-	public void start(Stage mainStage) {                
-		// create an ObservableList 
-		// from an ArrayList              
-		obsList = FXCollections.observableArrayList(                               
-				"Test Song");               
-		listView.setItems(obsList);  
+	
+	private ObservableList<String> obsList = FXCollections.observableArrayList();
+	
+	private List<List<String>> allSongs = new ArrayList<List<String>>();
+	
+	@FXML private ListView<String> listView;
+	@FXML private Button cancelButton;
+	@FXML private Button saveAddButton;
+	@FXML private Button saveEditButton;
+	@FXML private Button editButton;
+	@FXML private Button deleteButton;
+	@FXML private Text songText;
+	@FXML private Text artistText;
+	@FXML private Text albumText;
+	@FXML private Text yearText;
+	@FXML private Text errorText;
+	@FXML private TextField newSong;
+	@FXML private TextField newArtist;
+	@FXML private TextField newAlbum;
+	@FXML private TextField newYear;
+	
+	//hide all add/edit fields
+	private void hide(String type){
+		newSong.setVisible(false);
+		newArtist.setVisible(false);
+		newAlbum.setVisible(false);
+		newYear.setVisible(false);
+		cancelButton.setVisible(false);
+		errorText.setText("");
+		
+		if(type.equals("add") || type.equals("both")){
+			saveAddButton.setVisible(false);
+		}
+		
+		if(type.equals("edit") || type.equals("both")){
+			saveEditButton.setVisible(false);
+		}
+		
+		newSong.clear();
+		newArtist.clear();
+		newAlbum.clear();
+		newYear.clear();
 	}
+	
+	//show all add/edit fields
+	private void show(String type){
+		newSong.setVisible(true);
+		newArtist.setVisible(true);
+		newAlbum.setVisible(true);
+		newYear.setVisible(true);
+		cancelButton.setVisible(true);
+		deleteButton.setVisible(false);
+		editButton.setVisible(false);
+
+		
+		if(type.equals("add") || type.equals("both")){
+			saveAddButton.setVisible(true);
+		}
+		
+		if(type.equals("edit") || type.equals("both")){
+			saveEditButton.setVisible(true);
+		}
+	}
+	
+	//when song is clicked, display details
+	@FXML protected void songDetails() {
+    		int index = listView.getSelectionModel().getSelectedIndex();
+    		System.out.println(index);
+    		if(index == -1) {
+    			editButton.setVisible(false);
+    			deleteButton.setVisible(false);
+    		}
+    		else {
+    			songText.setText(allSongs.get(index).get(0));
+    			artistText.setText(allSongs.get(index).get(1));
+    			if(allSongs.get(index).get(2).equals("")) {
+    				albumText.setText("(empty)");
+    			}else {
+    				albumText.setText(allSongs.get(index).get(2));
+    			}
+    			if(allSongs.get(index).get(3).equals("")) {
+    				yearText.setText("(empty)");
+    			}else {
+    				yearText.setText(allSongs.get(index).get(3));
+    			}
+    		
+    			hide("both");
+    			editButton.setVisible(true);
+    			deleteButton.setVisible(true);
+    		}
+
+    }
+	
+	//show add song fields/buttons
+	//show all add Fields
+	@FXML
+	private void createScreen(ActionEvent event) throws IOException{
+		show("add");
+		
+	}
+	
+	//show all edit fields and prefill text boxes.
+	
+	//show edit song fields/buttons
+	@FXML 
+	private void editScreen(ActionEvent event) throws IOException{
+		show("edit");
+		
+		int index = listView.getSelectionModel().getSelectedIndex();
+		
+		newSong.setText(allSongs.get(index).get(0));
+		newArtist.setText(allSongs.get(index).get(1));
+		newAlbum.setText(allSongs.get(index).get(2));
+		newYear.setText(allSongs.get(index).get(3));
+	}
+	
+	//first method to run, hide all add/edit fields and populate listView
+	
+	//initialize screen 
+	public void start(Stage mainStage) throws IOException {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("songLib.txt"));
+			String line = br.readLine();
+			
+			while(line != null) {
+				ArrayList<String> song = new ArrayList<String>();
+				for(int j = 0; j<4;j++) {
+					song.add(line);
+					line = br.readLine();
+				}
+				allSongs.add(song);
+			}
+			br.close();
+			for(List<String> song : allSongs) {
+				obsList.add(song.get(0) + " - " + song.get(1));
+			}
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		hide("both");
+		editButton.setVisible(false);
+		deleteButton.setVisible(false);
+		listView.setItems(obsList);  
+		listView.getSelectionModel().select(0);
+		songDetails();
+		
+		listView
+		.getSelectionModel()
+		.selectedIndexProperty()
+		.addListener(
+				(obs, oldVal, newVal) -> 
+				songDetails());
+		
+	}
+
 
 	//create new song and add to list
-	public void create() {
-		//get details from screen
-		//add to obsList
+	@FXML 
+	private void add(ActionEvent event) throws IOException {
+		
+		if(newSong.getText().isEmpty()) {
+			errorText.setText("Song name is required.");
+			errorText.setVisible(true);
+		} else if(newArtist.getText().isEmpty()) {
+			errorText.setText("Artist name is required.");
+			errorText.setVisible(true);
+		}
+		else {
+			ArrayList<String> song = new ArrayList<String>();
+			
+			//get details from screen
+			song.add(newSong.getText());
+			song.add(newArtist.getText());
+			song.add(newAlbum.getText());
+			song.add(newYear.getText());
+			
+			//add to obsList
+			int index = getIndex(newSong.getText(), newArtist.getText());
+			
+			if(index != -1) {
+				allSongs.add(index, song);
+				obsList.add(index, newSong.getText() + " - " + newArtist.getText());
+				hide("add");
+				listView.getSelectionModel().select(index);
+				songDetails();
+			}
+		}
 	}
 	
-	//get data from list and display
-	public void read() {
-		//get song from screen
-		//read data from obsList and display
+	//find where to insert
+	public int getIndex(String song, String artist) {
+		int index = 0;
+		while(index < obsList.size()) {
+			int compare = obsList.get(index).compareTo(song + " - " + artist); 
+			System.out.println(compare);
+			if(compare == 0){
+				errorText.setText("This song already exists");
+				errorText.setVisible(true);
+				index = -1;
+				break;
+			}
+			if(compare > 0) {
+				break;
+			}
+			index++;
+		}
+		return index;
+	}
+	//save edited changes.
+	
+	//take current song and edit
+	@FXML 
+	private void edit(ActionEvent event) throws IOException {
+		int index = listView.getSelectionModel().getSelectedIndex();
+		if(newSong.getText().isEmpty()) {
+			errorText.setText("Song name is required.");
+		} else if(newArtist.getText().isEmpty()) {
+			errorText.setText("Artist name is required.");
+		}
+		else {
+			ArrayList<String> song = new ArrayList<String>();
+			
+			String songName = newSong.getText();
+			String artistName = newArtist.getText();
+			String albumName = newAlbum.getText();
+			String yearName = newYear.getText();
+			
+			//get details from screen
+			song.add(songName);
+			song.add(artistName);
+			song.add(albumName);
+			song.add(yearName);
+			
+			//add to obsList
+			ArrayList<String> oldSong = (ArrayList<String>) allSongs.get(index);
+			allSongs.remove(index);
+			obsList.remove(index);
+			
+			int newIndex = getIndex(songName, artistName);
+			
+			if(newIndex != -1) {
+				allSongs.add(newIndex, song);
+				obsList.add(newIndex, songName + " - " + artistName);
+				listView.getSelectionModel().select(newIndex);
+				hide("edit");
+				songDetails();
+			}else {
+				allSongs.add(index, oldSong);
+				obsList.add(index, oldSong.get(0) + " - " + oldSong.get(1));
+				listView.getSelectionModel().select(index);
+				errorText.setText("That song already exists");
+				errorText.setVisible(true);
+				show("edit");
+				newSong.setText(allSongs.get(index).get(0));
+				newArtist.setText(allSongs.get(index).get(1));
+				newAlbum.setText(allSongs.get(index).get(2));
+				newYear.setText(allSongs.get(index).get(3));
+			}
+						
+		}
 	}
 	
-	//edit song details and save
-	public void update() {
-		//get details from screen
-		//edit obsList
+	//hide the fields/buttons
+	@FXML 
+	private void cancel(ActionEvent event) throws IOException {
+		hide("both");
+		songDetails();
 	}
 	
-	//remove song from list
-	public void delete() {
-		//get song from screen
-		//remove from obsList
+	//remove from lists
+	@FXML 
+	private void delete(ActionEvent event) throws IOException{
+		Alert alert = new Alert(AlertType.CONFIRMATION, "Are you sure you want to delete?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+		alert.showAndWait();
+
+		if (alert.getResult() == ButtonType.YES) {
+		    //do stuff
+		
+			int index = listView.getSelectionModel().getSelectedIndex();
+			allSongs.remove(index);
+			obsList.remove(index);
+	
+			if(obsList.size() == 0) {
+				songText.setText("");
+				artistText.setText("");
+				albumText.setText("");
+				yearText.setText("");
+				return;
+			}
+			else if(obsList.size() == index) {
+				System.out.println("last item " + index + ", size: " + (obsList.size()-1));
+				index--;
+			}
+			
+			listView.getSelectionModel().select(index);
+			songDetails();
+		}
+	}
+
+	//save list out to file
+	public void saveFile() throws FileNotFoundException {
+		
+		if(obsList.size() > 0) {
+			PrintWriter out = new PrintWriter(new FileOutputStream("songlib.txt"));
+			
+			for(List<String> song : allSongs) {
+				for(String detail : song) {
+					System.out.println(detail);
+					out.println(detail);
+				}	
+			}
+			out.flush();
+			out.close();
+		}
+		System.exit(0);
 	}
 }
