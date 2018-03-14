a2f0f33abfbc595ada11561c678a78b9e93b851b
Jmuccino <jmuccino@rutgers.edu>
2018-03-14 18:59:55 -0400

    Queen coastClear() working.

--- Chess30/src/chess/Queen.java
+++ Chess30/src/chess/Queen.java
@@ -26,8 +26,101 @@ public class Queen extends Piece {
 	
 	@Override
 	protected boolean coastClear(int startRow, int startCol, int endRow, int endCol, ChessBoard board) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean properOrder = true;
+		
+		if(startRow == endRow) {
+			if(startCol > endCol) {
+				int temp = --startCol;
+				startCol = ++endCol;
+				endCol = temp;
+			}
+			else {
+				startCol++;
+				endCol--;
+			}
+			int i = startCol;
+			while(i <= endCol) {
+				if(board.getPiece(startRow, i) != null) {
+					return false;
+				}
+				i++;
+			}
+		}
+		else if(startCol == endCol){
+			System.out.println("else");
+			if(startRow > endRow) {
+				int temp = --startRow;
+				startRow = ++endRow;
+				endRow = temp;
+				System.out.println("sr"+startRow+"er" +endRow);
+			}
+			else {
+				startRow++;
+				endRow--;
+			}
+			int i = startRow;
+			while(i <= endRow) {
+				System.out.println(i);
+				if(board.getPiece(i, startCol) != null) {
+					return false;
+				}
+				i++;
+			}
+				
+		}
+		else //move is up/left, make it the same as down/right (eliminate endpoints)
+			if(startRow > endRow && startCol > endCol) {
+				int temp = --startRow;
+				startRow = ++endRow;
+				endRow = temp;
+				temp = --startCol;      
+				startCol = ++endCol;    
+				endCol = temp;          
+			}
+			//move is down/right, eliminate endpoints
+			else if(startRow < endRow && startCol < endCol) {
+				startRow++;
+				endRow--;
+				startCol++;
+				endCol--;
+			}
+			//move is up/right, make sme as down/left (elminate endpoints)
+			else if(startRow > endRow && startCol < endCol) {
+				properOrder = false;
+				int temp = --startRow;	
+				startRow = ++endRow;
+				endRow = temp;
+				temp = ++startCol;				
+				startCol = --endCol;
+				endCol = temp;
+				System.out.println("sr"+startRow+"er" +endRow);
+			}
+			//move is down/left, eliminate endpoints
+			else if(startRow < endRow && startCol > endCol) {
+				properOrder = false;
+				startRow++;
+				endRow--;
+				startCol--;
+				endCol++;
+			}else {
+				System.out.println("this should never happen");
+			}
+			int i = startRow;
+			int j = startCol;
+			
+				while(i <= endRow) {
+					if(board.getPiece(i, j) != null) {
+						return false;
+					}
+					i++;
+					if(properOrder) {
+						j++;
+					}else {
+						j--;
+					}
+				}
+		
+		return true;
 	}
 	
 	@Override
