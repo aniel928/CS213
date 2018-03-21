4df8acac364e3f7d225326ba30da7144044528d5
Anne Whitman <awhitman@docs.rutgers.edu>
2018-03-21 12:57:43 -0400

    Adding all javadocs

---
+++ Chess30/src/chess/PieceName.java
@@ -0,0 +1,54 @@
+package chess;
+
+/**
+* Possible names of chess pieces.
+* @author alh220
+* @author jmuccino
+*
+*/
+enum PieceName
+{
+	/**
+	 * Identifies piece as King 
+	 * @see King
+	 */
+	KING,
+	
+	/**
+	 * Identifies piece as Queen 
+	 * @see Queen
+	 */
+	QUEEN,
+	
+	/**
+	 * Identifies piece as Bishop 
+	 * @see Bishop
+	 */
+	BISHOP,
+	
+	/**
+	 * Identifies piece as Knight 
+	 * @see Knight
+	 */
+	KNIGHT, 
+	
+	/**
+	 * Identifies piece as Rook 
+	 * @see Rook
+	 */
+	ROOK, 
+	
+	/**
+	 * Identifies piece as Pawn 
+	 * @see Pawn
+	 */
+	PAWN, 
+	
+	/**
+	 * Identifies piece as GhostPawn 
+	 * @see GhostPawn
+	 */
+	GHOST;
+}
+
+
---
+++ Chess30/src/chess/Player.java
@@ -0,0 +1,20 @@
+package chess;
+
+/**
+ * Color of player for Chess game. Used primarily to determine who each piece belongs to or whose turn it is.
+ * @author alh220
+ * @author jmuccino
+ * 
+ */
+enum Player
+{
+	/**
+	 * Identifies something belong to the White player.
+	 */
+    WHITE, 
+
+	/**
+	 * Identifies something belong to the Black player.
+	 */
+    BLACK;
+}
